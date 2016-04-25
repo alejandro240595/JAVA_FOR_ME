d90c0bda49e37efe1947c541709830771f589eb6
alejandro240595 <ale240595@gmail.com>
2016-04-24 20:57:29 -0500

    actualizaciuon

---
+++ CLASE01/ejemplo2/src/clase/clase.java
@@ -0,0 +1,43 @@
+
+package clase;
+
+
+public class clase 
+{
+    double horaxdia;
+    double dia;
+    double pagoxdia;
+
+    public clase(double horaxdia, double dia, double pagoxdia) {
+        this.horaxdia = 0;
+        this.dia = 0;
+        this.pagoxdia = 0;
+    }
+
+    public double getHoraxdia() {
+        return horaxdia;
+    }
+
+    public void setHoraxdia(double horaxdia) {
+        this.horaxdia = horaxdia;
+    }
+
+    public double getDia() {
+        return dia;
+    }
+
+    public void setDia(double dia) {
+        this.dia = dia;
+    }
+
+    public double getPagoxdia() {
+        return pagoxdia;
+    }
+
+    public void setPagoxdia(double pagoxdia) {
+        this.pagoxdia = pagoxdia;
+    }
+    
+    
+    
+}
