8fed8760675904a3fc0c18693a156544e8d5b322
alejandro240595 <ale240595>
2016-05-29 17:23:53 -0500

    webeando

---
+++ Clase 4/AprendiendoJava/src/pe/egcc/prueba/prueba3.java
@@ -0,0 +1,23 @@
+
+package pe.egcc.prueba;
+
+/**
+ *
+ * @author Alumno
+ */
+public class prueba3 {
+
+  
+    public static void main(String[] args) {
+       int[] notas = new int[5];
+       
+        System.out.println("for indexado"); // usa el indice del arreglo para ingresar a los datos del mismo
+        for (int i = 0; i < notas.length; i++)
+        {
+            int nota = notas[i];
+            System.out.println("nota");
+            
+        }
+    }
+    
+}
